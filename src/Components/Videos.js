9e2cd3e2b4def84d522f9b46d7e533937a588ed9
mohajon33 <mohajon33@gmail.com>
2022-01-29 21:25:43 +0600

    implement component routing

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "../Styles/Videos.module.css";
 import Video from "./Video";
 
 const Videos = () => {
   return (
     <div className={classes.videos}>
-      <Video />
-      <Video />
-      <Video />
+      <Link to="/Quiz">
+        <Video />
+      </Link>
+      <Link to="/Quiz">
+        <Video />
+      </Link>
+      <Link to="/Quiz">
+        <Video />
+      </Link>
+      <Link to="/Quiz">
+        <Video />
+      </Link>
+      <Link to="/Quiz">
+        <Video />
+      </Link>
+      <Link to="/Quiz">
+        <Video />
+      </Link>
     </div>
   );
 };
